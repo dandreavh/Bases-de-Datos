b5876e3c357d22852a8d6fe2bfee101e125480b6
dandreavh <alum.dvieirah.2021@iesalixar.org>
2022-04-01 11:37:24 +0200

    SQL data DDL - Proyecto

---
+++ Version2/AntiguosAlumnos_202204011127.sql
@@ -0,0 +1,2 @@
+INSERT INTO `academia-atentos-db`.AntiguosAlumnos (idAlumno,idPersona,idTutor,FechaBaja) VALUES
+	 (5555,5555,5,'2022-03-29');
---
+++ Version2/HistoricoPagos_202204011134.sql
@@ -0,0 +1,2 @@
+INSERT INTO `academia-atentos-db`.HistoricoPagos (idAlumno,CantidadPagada,FechaPago) VALUES
+	 (5555,15.00,'2022-03-29');
